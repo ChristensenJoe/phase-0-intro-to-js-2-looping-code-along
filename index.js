72c1738abfdd5ee675288f0f41afa1e355d7db44
Joe Christensen <joechristensenwork@gmail.com>
2021-06-13 11:31:32 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(eventNames, event) {
+    const arr = [];
+    for(let i = 0; i < eventNames.length; i++) {
+        arr.push(`Thank you, ${eventNames[i]}, for the wonderful ${event} gift!`);
+    }
+    return arr;
+}
+
+function countDown(n) {
+    for(let i = n; i >= 0; i--) {
+        console.log(i);
+    }
+}
